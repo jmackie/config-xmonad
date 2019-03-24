0a23a9e4d449aa9213a647b8e153ef45343a0a11
Jordan Mackie <jmackie@protonmail.com>
2019-03-24 08:21:32 +0000

    More tweaking

--- xmonad.hs
+++ xmonad.hs
@@ -8,7 +8,6 @@ module Main (main) where
 import Prelude
 
 import qualified Data.Map as M
-import qualified Language.Haskell.TH.Quote.Text as TH
 
 import XMonad
 import XMonad.Config.Desktop (desktopConfig)
@@ -20,7 +19,7 @@ import qualified Colors
 
 
 main :: IO ()
-main = xmonad =<< 
+main = xmonad =<<
     statusBar "xmobar ~/.xmonad/xmobar.hs" myPP toggleStrutsKey myXConfig
 
 
@@ -28,11 +27,16 @@ myXConfig :: XConfig _
 myXConfig = desktopConfig
     { terminal = "alacritty"
     , modMask = mod1Mask -- Alt
+    , workspaces = myWorkspaces
     , manageHook = myManageHook <+> manageHook desktopConfig
-    , keys = myKeys <> XMonad.keys desktopConfig 
+    , keys = myKeys <> XMonad.keys desktopConfig
     , logHook = dynamicLogString def >>= xmonadPropLog
     , focusedBorderColor = Colors.brightGreen
     }
+
+
+myWorkspaces :: [String]
+myWorkspaces = ["1:term","2:web","3","4","5","6","7","8","9","0","-","="]
 
 
 -- | Stdin pretty-printer for xmobar.
@@ -40,133 +44,50 @@ myPP :: PP
 myPP = def
 
 
+-- |
+--
+-- To get a className, run the following then click on the window
+-- of interest:
+--
+-- > xprop | grep WM_CLASS
 myManageHook :: ManageHook
 myManageHook = composeOne
-    [ className =? "gxmessage" -?> doCenterFloat
-    , isDialog -?> doCenterFloat
+    [ className =? "Firefox"   -?> doShift "web"
+    , className =? "Alacritty" -?> doShift "term"
+
+    , className =? "Gxmessage" -?> doCenterFloat
+    , isDialog                 -?> doCenterFloat
+
     , transience -- Move transient windows to their parent
     ]
 
 
 myKeys :: XConfig Layout -> M.Map (ButtonMask, KeySym) (X ())
-myKeys XConfig{ modMask } =
-    [ ( (modMask, xK_p )
+myKeys XConfig{ terminal, modMask } =
+    [ -- Use a prettier dmenu
+      ( (modMask, xK_p )
       , spawn dmenu
       )
-    , ( (modMask .|. shiftMask, xK_slash )
-      , spawn ("echo \"" <> help <> "\" | gxmessage -fn Hack -center -file -")
+      -- TODO: It would be nice if I could make this use the focused terminal's
+      -- wd
+    , ( (modMask .|. shiftMask, xK_Return)
+      , spawn terminal
       )
+      -- Turn off
+    --, ( (modMask .|. shiftMask, xK_p )
+    --  , ???
+    --  )
     ]
+  where
+    dmenu :: String
+    dmenu = "dmenu_run \
+        \-nb \"#000000\" \
+        \-nf \"#dddddd\" \
+        \-sb \"#14ffff\" \
+        \-sf \"#000000\" \
+        \-fn \"Hack:bold:pixelsize=36\" \
+        \-p Run"
 
 
 toggleStrutsKey :: XConfig a -> (KeyMask, KeySym)
 toggleStrutsKey XConfig { modMask } = (modMask, xK_b)
-
-
-dmenu :: String
-dmenu = "dmenu_run \
-    \-nb \"#000000\" \
-    \-nf \"#dddddd\" \
-    \-sb \"#14ffff\" \
-    \-sf \"#000000\" \
-    \-fn \"Hack:bold:pixelsize=36\" \
-    \-p Run"
-
-
-help :: String
-help = [TH.text|# Custom key bindings
-
-mod-shift-slash:
-    Print this help message
-
-# Default key bindings
-
-mod-shift-return:
-    Launch terminal
-
-mod-p:
-    Launch dmenu
-
-mod-shift-p:
-    Launch gmrun
-
-mod-shift-c:
-    Close the focused window
-
-mod-space:
-    Rotate through the available layout algorithms
-
-mod-shift-space:
-    Reset the layouts on the current workspace to default
-
-mod-n:
-    Resize viewed windows to the correct size
-
-mod-tab:
-    Move focus to the next window
-
-mod-shift-tab:
-    Move focus to the previous window
-
-mod-j:
-    Move focus to the next window
-
-mod-k:
-    Move focus to the previous window
-
-mod-m:
-    Move focus to the master window
-
-mod-return:
-    Swap the focused window and the master window
-
-mod-shift-j:
-    Swap the focused window with the next window
-
-mod-shift-k:
-    Swap the focused window with the previous window
-
-mod-h:
-    Shrink the master area
-
-mod-l:
-    Expand the master area
-
-mod-t:
-    Push window back into tiling
-
-mod-comma:
-    Increment the number of windows in the master area
-
-mod-period:
-    Deincrement the number of windows in the master area
-
-mod-shift-q:
-    Quit xmonad
-
-mod-q:
-    Restart xmonad
-
-mod-[1..9]:
-    Switch to workspace N
-
-mod-shift-[1..9]:
-    Move client to workspace N
-
-mod-{w,e,r}:
-    Switch to physical/Xinerama screens 1, 2, or 3
-
-mod-shift-{w,e,r}:
-    Move client to screen 1, 2, or 3
-
-mod-button1:
-    Set the window to floating mode and move by dragging
-
-mod-button2:
-    Raise the window to the top of the stack
-
-mod-button3:
-    Set the window to floating mode and resize by dragging
-
-https://xmonad.org/manpage.html
-|]
