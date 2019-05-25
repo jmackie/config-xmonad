4686f114b74d851d7b84e38c7296cd3cc5024154
Jordan Mackie <jmackie@protonmail.com>
2019-05-25 20:17:32 +0100

    Try and get new terminals opening in the same dir
    And some general tweaking...

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE OverloadedLists       #-}
 {-# LANGUAGE PartialTypeSignatures #-}
@@ -6,28 +7,36 @@ module Main (main) where
 
 import Prelude
 
+import Control.Arrow ((>>>))
+import qualified Data.List as List
 import qualified Data.Map as M
+import System.Directory (getSymbolicLinkTarget)
 
 import Graphics.X11.ExtraTypes.XF86
-    (xF86XK_MonBrightnessDown, xF86XK_MonBrightnessUp)
+  (xF86XK_MonBrightnessDown, xF86XK_MonBrightnessUp)
+
 import XMonad
 import qualified XMonad.Actions.CycleWS as CycleWS
 import XMonad.Config.Desktop (desktopConfig)
-import XMonad.Hooks.DynamicLog (PP, dynamicLogString, statusBar, xmonadPropLog)
+import XMonad.Hooks.DynamicLog
+  (PP(..), dynamicLogString, statusBar, xmobarPP, xmonadPropLog)
 import XMonad.Hooks.ManageHelpers
-    (composeOne, doCenterFloat, isDialog, transience, (-?>))
+  (composeOne, doCenterFloat, isDialog, pid, transience, (-?>))
+import qualified XMonad.Layout.WorkspaceDir as WorkspaceDir
+import qualified XMonad.StackSet as StackSet
 import qualified XMonad.Util.Brightness as Brightness
 
 import qualified Colors
 
 
 main :: IO ()
-main = xmonad =<<
+main = 
+  xmonad =<<
     statusBar "xmobar ~/.xmonad/xmobar.hs" myPP toggleStrutsKey myXConfig
 
-
 myXConfig :: XConfig _
-myXConfig = desktopConfig
+myXConfig = 
+  desktopConfig
     { terminal = "alacritty"
     , modMask = mod1Mask -- Alt
     , workspaces = myWorkspaces
@@ -40,19 +49,19 @@ myXConfig = desktopConfig
     , borderWidth = 2
     }
 
-
 myWorkspaces :: [String]
 myWorkspaces = ["1","2","3","4","5","6","7","8","9","0","-","="]
-
 
 myStartupHook :: X ()
 myStartupHook = pure ()
 
-
 -- | Stdin pretty-printer for xmobar.
 myPP :: PP
-myPP = def
-
+myPP = 
+  xmobarPP 
+    { ppOrder = \(ws:_:t:_) -> [ws, t]
+    , ppExtras = []
+    }
 
 -- |
 --
@@ -62,48 +71,71 @@ myPP = def
 -- > xprop | grep WM_CLASS
 myManageHook :: ManageHook
 myManageHook = composeOne
-    [ className =? "Gxmessage" -?> doCenterFloat
-    , isDialog                 -?> doCenterFloat
+  [ className =? "Gxmessage" -?> doCenterFloat
+  , isDialog                 -?> doCenterFloat
 
-    , transience -- Move transient windows to their parent
-    ]
-
+  , transience -- Move transient windows to their parent
+  ]
 
 myKeys :: XConfig Layout -> M.Map (ButtonMask, KeySym) (X ())
 myKeys XConfig{ terminal, modMask } =
-    [ -- Use a prettier dmenu
-      ( (modMask, xK_p)
-      , spawn dmenu
-      )
-      -- mod+tab cycles between workspaces
-    , ( (modMask, xK_Tab)
-      , cycleWS
-      )
-      -- TODO: It would be nice if I could make this 
-      -- use the focused terminal's working dir
-    , ( (modMask .|. shiftMask, xK_Return)
-      , spawn terminal
-      )
+  [ -- Use a prettier dmenu
+    ( (modMask, xK_p)
+    , spawn dmenu
+    )
+    -- mod+tab cycles between workspaces
+  , ( (modMask, xK_Tab)
+    , cycleWS
+    )
+    -- TODO: It would be nice if I could make this 
+    -- use the focused terminal's working dir
+  , ( (modMask .|. shiftMask, xK_Return)
+    , spawnTerminalInSameDirectory terminal
+    )
 
-    , ((0, xF86XK_MonBrightnessUp),   Brightness.increase)
-    , ((0, xF86XK_MonBrightnessDown), Brightness.decrease)
+    -- WIP: Prompt to change workspace directory
+  , ( (modMask .|. shiftMask, xK_x)
+    , WorkspaceDir.changeDir def
+    --                       ^^^ TODO: configure this better
+    )
 
-      -- Turn off
-      --, ( (modMask .|. shiftMask, xK_p )
-      --  , ???
-      --  )
-    ]
+  , ((0, xF86XK_MonBrightnessUp),   Brightness.increase)
+  , ((0, xF86XK_MonBrightnessDown), Brightness.decrease)
+  ]
   where
-    cycleWS = CycleWS.moveTo CycleWS.Next CycleWS.NonEmptyWS
+  cycleWS = CycleWS.moveTo CycleWS.Next CycleWS.NonEmptyWS
 
-    dmenu = "dmenu_run \
-        \-nb \"#000000\" \
-        \-nf \"#dddddd\" \
-        \-sb \"#14ffff\" \
-        \-sf \"#000000\" \
-        \-fn \"Hack:bold:pixelsize=36\" \
-        \-p Run"
+  dmenu = 
+    "dmenu_run \
+       \-nb \"#000000\" \
+       \-nf \"#dddddd\" \
+       \-sb \"#14ffff\" \
+       \-sf \"#000000\" \
+       \-fn \"Hack:bold:pixelsize=36\" \
+       \-p Run"
 
+getFocusedWindow :: WindowSet -> Maybe Window
+getFocusedWindow 
+ = fmap StackSet.focus 
+ . StackSet.stack 
+ . StackSet.workspace 
+ . StackSet.current
+
+spawnTerminalInSameDirectory :: String -> X ()
+spawnTerminalInSameDirectory terminal = 
+  withWindowSet $ getFocusedWindow >>> \case
+    Nothing  -> spawn terminal
+    Just win ->
+      runQuery pid win >>= \case
+        Nothing -> spawn terminal
+        Just pid' -> do
+          name <- io $ readFile ("/proc/" <> show pid' <> "/cmdline")
+          if terminal `List.isPrefixOf` name 
+             then do
+                 wd <- io $ getSymbolicLinkTarget ("/proc/" <> show pid' <> "/cwd")
+                 spawn (terminal <> " --working-directory=" <> wd)
+
+             else spawn terminal
 
 -- | @mod-b@ toggles struts.
 toggleStrutsKey :: XConfig a -> (KeyMask, KeySym)
