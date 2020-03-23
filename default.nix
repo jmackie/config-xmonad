8fbec645160d9253d9aa6ec2f390651e7238dfb0
Jordan Mackie <jmackie@protonmail.com>
2020-03-23 09:29:29 +0000

    Filter source so nix doesn't do a full rebuild every time

--- default.nix
+++ default.nix
@@ -1,8 +1,22 @@
 { pkgs ? import ./nix/nixpkgs { }, ghc ? "ghc865" }:
 let
   haskellPackages = pkgs.haskell.packages."${ghc}";
-  src = builtins.path { name = "config-xmonad"; path = ./.; };
-  drv = haskellPackages.callCabal2nixWithOptions "config-xmonad" src "--no-haddock --no-check" { };
+
+  # Be quite strict about filtering files to minimize rebuilding.
+  # Otherwise this we recompiles on every startup, which is slow
+  # and can also hang if the network is unavailable!
+  srcFilter = f: type:
+    pkgs.lib.hasPrefix (toString ./src) (toString f)
+    || pkgs.lib.elem (toString f)
+    (map toString [ ./config-xmonad.cabal ./xmobar.hs ./xmonad.hs ]);
+
+  src = pkgs.lib.cleanSourceWith {
+    filter = srcFilter;
+    src = ./.;
+    name = "config-xmonad";
+  };
+  drv = haskellPackages.callCabal2nixWithOptions "config-xmonad" src
+    "--no-haddock --no-check" { };
   shell = let
     ghcide-nix = import ./nix/ghcide-nix { inherit pkgs; };
     ormolu = import ./nix/ormolu { inherit pkgs; };
